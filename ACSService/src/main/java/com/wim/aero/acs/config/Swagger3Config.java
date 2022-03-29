008adca0077f68838ec86776fbdcd061783a64a8
EllieYY <xuanh_w@sina.com>
2022-03-29 16:58:17 +0800

    add knife4j

--- ACSService/src/main/java/com/wim/aero/acs/config/Swagger3Config.java
+++ ACSService/src/main/java/com/wim/aero/acs/config/Swagger3Config.java
@@ -1,42 +1,92 @@
-//package com.wim.aero.acs.config;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import springfox.documentation.service.ApiInfo;
-//import springfox.documentation.service.Contact;
-//import springfox.documentation.service.VendorExtension;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
-//
-//import java.util.ArrayList;
-//
-///**
-// * @Description : api文档配置类
-// * @Author : Ellie
-// * @Date : 2019/2/18
-// */
-//@Configuration
-////@EnableOpenApi
-//public class Swagger3Config {
-//
-//    //配置Swagger的Docket的bean实例
-//    @Bean
-//    public Docket docket() {
-//        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()); // 配置Swagger信息
-//    }
-//
-//    // 配置Swagger信息
-//    private ApiInfo apiInfo() {
-//        return new ApiInfo(
-//                "门禁系统",
-//                "门禁系统服务AB API文档",
-//                "1.0",
-//                "iafc",//团队信息的url
-//                new Contact("Ellie", "", ""),
-//                "Apache 2.0",
-//                "http://www.apache.org/licenses/LICENSE-2.0",
-//                new ArrayList<VendorExtension>());
-//    }
-//}
+package com.wim.aero.acs.config;
+
+import io.swagger.annotations.Api;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseBuilder;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.Response;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description : api文档配置类
+ * @Author : Ellie
+ */
+@Configuration
+@EnableOpenApi
+@Slf4j
+public class Swagger3Config {
+    @Value("${spring.profiles.active:''}")
+    private String active;
+    @Value("${server.port}")
+    private int port;
+
+    @Bean
+    @SneakyThrows
+    public Docket shopManager() {
+        Docket docket = new Docket(DocumentationType.OAS_30)
+                // 非生产环境才开启swagger
+//                .enable(!active.equalsIgnoreCase(Constant.SPRING_PROFILES_ACTIVE_PRO))
+                .groupName("group1")
+                .apiInfo(apiInfo())
+                // 设置自定义返回消息体
+                .globalResponses(HttpMethod.GET, globalResponse())
+                .globalResponses(HttpMethod.POST, globalResponse())
+                .select()
+                // 扫描带有@Api注解的类
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                // 扫描的基础路径 any()：任何请求都扫描；none()：任何请求都不扫描；ant("/manager")：扫描该路径下的请求
+                .paths(PathSelectors.any())
+                .build();
+
+        String ipAddress = InetAddress.getLocalHost().getHostAddress();
+        // 控制台输出Swagger3接口文档地址
+        log.info("Swagger3接口文档地址: http://{}:{}/swagger-ui.html", ipAddress, port);
+        // 控制台输出Knife4j增强接口文档地址
+        log.info("Knife4j增强接口文档地址: http://{}:{}/doc.html", ipAddress, port);
+
+        return docket;
+    }
+
+
+    private ApiInfo apiInfo(){
+        String title = "门禁业务系统";
+        String description = "API接口文档";
+        String version = "1.0.0";
+        String license = "";
+        String termsOfServiceUrl = "";
+        Contact contact = new Contact("Ellie", "", "");
+        return new ApiInfoBuilder()
+                .title(title)
+                .description(description)
+                .version(version)
+                .license(license)
+                .termsOfServiceUrl(termsOfServiceUrl)
+                .contact(contact)
+                .build();
+    }
+
+    private List<Response> globalResponse(){
+        List<Response> responseList = new ArrayList<>();
+        responseList.add(new ResponseBuilder().code("401").description("未认证").build());
+        responseList.add(new ResponseBuilder().code("403").description("请求被禁止").build());
+        responseList.add(new ResponseBuilder().code("404").description("找不到资源").build());
+        return responseList;
+    }
+}
