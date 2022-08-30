66dfe392ded03aecfd4bc8d11e7af9dc2dcbb379
EllieYY <xuanh_w@sina.com>
2022-08-30 16:56:15 +0800

    change acr mode check.

--- ACSService/src/main/java/com/wim/aero/acs/model/request/AcrRequestInfo.java
+++ ACSService/src/main/java/com/wim/aero/acs/model/request/AcrRequestInfo.java
@@ -19,6 +19,6 @@ public class AcrRequestInfo extends TaskRequest {
     @ApiModelProperty(value = "控制器id")
     private int scpId;
 
-    @ApiModelProperty(value = "模式值 1 - 禁用  2 - 常开 3 - 常关 4 - 工程号开门 5 - 只刷卡 6 - 只PIN码 7 - 刷卡加PIN码 8 - 刷卡或PIN码", example = "")
+    @ApiModelProperty(value = "模式值 1 - 禁用  2 - 常开 3 - 常关 4 - 工程号开门 5 - 只刷卡 6 - 只PIN码 7 - 刷卡加PIN码 8 - 刷卡或PIN码等", example = "")
     private int command;
 }
--- ACSService/src/main/java/com/wim/aero/acs/protocol/device/reader/AcrMode.java
+++ ACSService/src/main/java/com/wim/aero/acs/protocol/device/reader/AcrMode.java
@@ -3,7 +3,7 @@ package com.wim.aero.acs.protocol.device.reader;
 import java.util.Arrays;
 
 public enum AcrMode {
-//    DISABLE(1),
+    DISABLE(1),
     UNLOCK(2),
     LOCKED(3),
     FACILITY_CODE_ONLY(4),
@@ -11,6 +11,15 @@ public enum AcrMode {
     PIN_ONLY(6),
     CARD_AND_PIN(7),
     CARD_OR_PIN(8),
+    ACR_F_2CARD_CLEAR(16),
+    ACR_F_2CARD(17),
+    ACR_FE_NO_ARQ_CLEAR(26),
+    ACR_FE_NO_ARQ(27),
+    ACR_FE_LINK_MODE(29),
+    ACR_FE_LINK_MODE_CLEAR(30),
+    EX_FEATURE_CHANGE(31),
+    ACR_FE_LINK_MODE_ALT(32),
+    ACR_FE_LINK_MODE_ALT_CLEAR(33),
     UNKNOWN(-1);
 
 
